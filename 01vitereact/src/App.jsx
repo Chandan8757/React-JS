7607ece441577adac7263e3ef64c9b97fa4ffa7e
Chandan8757 <ck846747@gmail.com>
2024-09-03 14:27:40 +0530

    React Js

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+  const Username = "chai aur code"
+
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react {Username}</h1>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
